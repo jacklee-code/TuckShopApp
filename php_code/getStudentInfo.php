e9453f656418020354017367341bce6233f90657
Jack Lee <jacklee-code>
2022-05-04 21:43:08 +0800

    Optimize & Completed getStudentInfo.php

--- php_code/getStudentInfo.php
+++ php_code/getStudentInfo.php
@@ -9,27 +9,40 @@
 
     try {
         $userid = loginAndGetUserId($db, $username, $password);
-        if (strlen($userid) < 1) {
-            http_response_code(403);
-            return;
-        } else {
+        if (strlen($userid) < 1)
+            callForbidden();
 
-            if (isStudent($db, $userid)) {
-                $sql = "SELECT a.UserId, a.Username, a.Fullname, a.Balance, t.TypeName AS AccountType FROM Accounts AS a, AccountType AS t WHERE a.UserId=:userid AND a.TypeId=t.TypeId;";
+        else {
+            $type = getUserTypeStringLower($db, $userid);
+            if ($type == "student") {
+                $sql = "SELECT a.UserId, a.Username, a.Fullname, a.Balance, t.TypeName AS AccountType 
+                        FROM Accounts AS a, AccountType AS t 
+                        WHERE a.UserId=:userid AND a.TypeId=t.TypeId;";
+
                 $statement = $db->prepare($sql);
                 $statement->bindParam(":userid", $userid, PDO::PARAM_INT);
                 $statement->execute();
                 $results = $statement->fetch(PDO::FETCH_ASSOC);
-            } else if (isParent($db, $userid)) {
-                $sql = "SELECT a.UserId, a.Username, a.Fullname, a.Balance, t.TypeName AS AccountType FROM Accounts AS a, AccountType AS t, Linkage AS l 
+            } else if ($type == "parent") {
+                $sql = "SELECT a.UserId, a.Username, a.Fullname, a.Balance, t.TypeName AS AccountType 
+                        FROM Accounts AS a, AccountType AS t, Linkage AS l 
                         WHERE a.UserId = l.StudentId AND l.ParentId = :userid AND a.TypeId=t.TypeId;";
+
                 $statement = $db->prepare($sql);
                 $statement->bindParam(":userid", $userid, PDO::PARAM_INT);
                 $statement->execute();
                 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-            } else if (isTeacher($db, $userid)) {
+            } else if ($type == "teacher") {
+                $sql = "SELECT a.UserId, a.Username, a.Fullname, a.Balance, t.TypeName AS AccountType 
+                        FROM Accounts AS a, AccountType AS t
+                        WHERE a.TypeId = t.TypeId AND t.TypeId = 1;";
 
-            }
+                $statement = $db->prepare($sql);
+                $statement->bindParam(":userid", $userid, PDO::PARAM_INT);
+                $statement->execute();
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            } else
+                callForbidden();
         }
 
 
